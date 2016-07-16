2765e9c6ea525e09decbd14664168f347d08914f
qincheng <bryant.qin@gmail.com>
2016-07-17 01:24:47 +0200

    refresh after gitignore update

---
+++ web/js/tinymce/jscripts/tiny_mce/plugins/jbimages/ci/application/language/chinese/jbstrings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['jb_blankpage_message'] = "无法连接到服务器";
+
+/* End of file jbstrings_lang.php */
+/* Location: ./application/language/english/jbstrings_lang.php */
---
+++ web/js/tinymce/jscripts/tiny_mce/plugins/jbimages/langs/ch_dlg.js
@@ -0,0 +1,25 @@
+/**
+ * Justboil.me - a TinyMCE image upload plugin
+ * jbimages/langs/en_dlg.js
+ *
+ * Released under Creative Commons Attribution 3.0 Unported License
+ *
+ * License: http://creativecommons.org/licenses/by/3.0/
+ * Plugin info: http://justboil.me/
+ * Author: Viktor Kuzhelnyi
+ *
+ * Version: 2.3 released 23/06/2013
+ */
+
+ tinyMCE.addI18n('ch.jbimages_dlg',{
+	title : '从您的电脑里选一张图片',
+	select_an_image : '选择图片',
+	upload_in_progress : '正在上传中',
+	upload_complete : '上传成功',
+	upload : '上传',
+	longer_than_usual : '速度有点慢',
+	maybe_an_error : '发生错误了',
+	view_output : '错误代码',
+	
+	lang_id : 'chinese' /* php-side language files are in: ci/application/language/{lang_id}; and in ci/system/language/{lang_id} */
+});
